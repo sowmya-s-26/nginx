15790e4ff3690739116ca91464a029def2377826
Sowmya S <sowmyashankar1998@gmail.com>
2025-01-10 14:54:13 +0530

    nginx project

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+
+const app = express();
+
+app.get("/", (req, res)=>{
+    res.send("Hey!")
+})
+
+app.listen(3000, ()=>{
+    console.log("APP Started on port 3000!")
+})
